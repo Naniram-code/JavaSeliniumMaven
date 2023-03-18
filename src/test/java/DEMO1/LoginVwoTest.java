7d4d261cf9351ae9348b6f8e285f836bcf4f6560
Naniram-code <NaniramBhujel@gmail.com>
2023-03-17 21:24:20 -0400

    update 03/18/2023 updated

--- src/test/java/DEMO1/LoginVwoTest.java
+++ src/test/java/DEMO1/LoginVwoTest.java
@@ -30,14 +30,13 @@ public class LoginVwoTest {
         driver.get("https://app.vwo.com");
         driver.manage().window().maximize();
     }
-    @Test(priority =1,enabled = false)
+    @Test(priority =1,enabled =false)
     public void NewRegistration() throws InterruptedException {
-       // Actions actions = new Actions(driver);
-
         driver.findElement(By.linkText("Start a free trial")).click();
         WebElement emailadd=driver.findElement(By.tagName("input"));
         //emailadd.sendKeys("Abc4321@gmail.com");//(1st user created)
-        emailadd.sendKeys("Abcd4321@gmail.com");//(2nd user created)
+        //emailadd.sendKeys("Abcd4321@gmail.com");//(2nd user created)
+        emailadd.sendKeys("Abcd34321@gmail.com");//(3nd user created)
         driver.findElement(By.tagName("button")).click();
        WebElement name=driver.findElement(By.xpath("//*[@id='page-v1-fname']"));
        Actions actions = new Actions(driver);
@@ -51,6 +50,10 @@ public class LoginVwoTest {
         actions.sendKeys(Psw,"abcedrg@#$$$65").build().perform();
         WebElement clickbutton=driver.findElement(By.tagName("button"));
         actions.moveToElement(clickbutton).doubleClick();
+        String ActualTitle=driver.getTitle();
+        System.out.println(ActualTitle);
+        String ExpectedTitle="Get Started with Free Trial | VWO";
+        Assert.assertEquals(ActualTitle,ExpectedTitle);
         Thread.sleep(5000);
         driver.close();
 
@@ -64,22 +67,24 @@ public class LoginVwoTest {
         password.sendKeys("ify@1tt23");
 
         driver.findElement(By.id("js-login-btn")).click();
-        WebElement errorMsg = driver.findElement(By.id("js-notification-box-msg"));
-        Assert.assertEquals(errorMsg.getText(), "Your email, password, IP address or location did not match");
+        String ActualErrorMsg = driver.findElement(By.id("js-notification-box-msg")).getText();
+        String ExpectedErroeMsg="Your email, password, IP address or location did not match";
+        Assert.assertEquals(ActualErrorMsg,ExpectedErroeMsg);
         Thread.sleep(2000);
         driver.close();}
 
     @Test(priority = 3)
     public void loginPositiveTest() throws InterruptedException {
         WebElement email = driver.findElement(By.id("login-username"));
-        //email.sendKeys("vwouser@gmail.com");
+        //email.sendKeys("vwouser@gmail.com");//(User 1)
         email.sendKeys("Abc4321@gmail.com");
         WebElement password = driver.findElement(By.name("password"));
-        //password.sendKeys("!@#$%^&*()Abc");
+        //password.sendKeys("!@#$%^&*()Abc");(psw user 1)
         password.sendKeys("abcedrg@#$$$6");
         driver.findElement(By.id("js-login-btn")).click();
-        String loginpageTitle = driver.getTitle();
-        Assert.assertEquals(loginpageTitle, "Login - VWO");
+        String ActualloginpageTitle = driver.getTitle();
+        String ExpectedloginpageTitle="Login - VWO";
+        Assert.assertEquals(ActualloginpageTitle,ExpectedloginpageTitle);
         Thread.sleep(2000);
         driver.close();}
     @AfterTest
